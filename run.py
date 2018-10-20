8e20c1c83e6d841d38491269bcda5fa891080496
James <jimiwhizz@gmail.com>
2018-10-20 15:54:27 +0100

    applied styling to emulate jeopardy game board

--- run.py
+++ run.py
@@ -74,4 +74,8 @@ if __name__ == '__main__':
     port = 8080,
     debug = True)
 
+# if __name__ == '__main__':
+#     app.run(host = os.getenv('IP', '0.0.0.0'),
+#     port = os.getenv('PORT', 8080),
+#     debug = True)
     
